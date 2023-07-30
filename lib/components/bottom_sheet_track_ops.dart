9ee8ecfdab54fc99220b131aaa510d267e07ead9
sqz269 <yin2690855381@gmail.com>
2023-07-30 10:54:17 -0500

    Removed unnessary container

--- lib/components/bottom_sheet_track_ops.dart
+++ lib/components/bottom_sheet_track_ops.dart
@@ -15,97 +15,108 @@ class BottomSheetTrackOps extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Wrap(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: ListTile(
-              leading: Image.network(
-                albumData.thumbnail!.medium!.url!,
-                fit: BoxFit.cover,
-                width: 24,
-                height: 24,
-              ),
-              title: RichText(
-                text: TextSpan(
-                  text: trackData.name!.default_!,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                  children: [
-                    TextSpan(
-                      text: '\n${albumData.albumName!.default_!}',
-                      style: Theme.of(context).textTheme.labelMedium,
-                    ),
-                  ],
-                ),
-              ),
-              onTap: null,
+    return Wrap(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: ListTile(
+            leading: Image.network(
+              albumData.thumbnail!.medium!.url!,
+              fit: BoxFit.cover,
+              width: 24,
+              height: 24,
             ),
+            title: RichText(
+              text: TextSpan(
+                text: trackData.name!.default_!,
+                style: Theme.of(context).textTheme.bodyLarge,
+                children: [
+                  TextSpan(
+                    text: '\n${albumData.albumName!.default_!}',
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ),
+            onTap: null,
           ),
-          const Divider(
-            height: 1,
-          ),
-          ListTile(
-            leading: Icon(Icons.playlist_play),
-            title: Text('Play next'),
-            onTap: () {
-              QueueController.to.addTrackById(trackData.id!).then(
-                (value) {
-                  Navigator.of(context).pop();
-                  // MobileMiniplayerBarController.to.expand();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Added to queue'),
-                      behavior: SnackBarBehavior.floating,
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.queue_music),
-            title: Text('Add to queue'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.playlist_add),
-            title: Text('Save to playlist'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.interpreter_mode),
-            title: Text('Go to artist'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.quiz),
-            title: Text('[DEBUG] Play in HLS Audio Test Page'),
-            onTap: () {
-              Navigator.of(context).pop();
-              Get.toNamed(
-                '/audio_test/${trackData.id!}',
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.copy),
-            title: Text('[DEBUG] Copy track UUID to clipboard'),
-            onTap: () {
-              Clipboard.setData(
-                ClipboardData(text: trackData.id!),
-              );
-              Navigator.of(context).pop();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Copied track UUID to clipboard'),
-                  behavior: SnackBarBehavior.floating,
-                ),
-              );
-            },
-          )
-        ],
-      ),
+        ),
+        const Divider(
+          height: 1,
+        ),
+        ListTile(
+          leading: Icon(Icons.playlist_play),
+          title: Text('Play next'),
+          onTap: () {
+            QueueController.to.addTrackById(trackData.id!).then(
+              (value) {
+                Navigator.of(context).pop();
+                // MobileMiniplayerBarController.to.expand();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Added to queue'),
+                    behavior: SnackBarBehavior.floating,
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.queue_music),
+          title: Text('Add to queue'),
+          onTap: () {
+            QueueController.to.addTrackById(trackData.id!).then(
+              (value) {
+                Navigator.of(context).pop();
+                // MobileMiniplayerBarController.to.expand();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Added to queue'),
+                    behavior: SnackBarBehavior.floating,
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.playlist_add),
+          title: Text('Save to playlist'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.interpreter_mode),
+          title: Text('Go to artist'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.quiz),
+          title: Text('[DEBUG] Play in HLS Audio Test Page'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Get.toNamed(
+              '/audio_test/${trackData.id!}',
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.copy),
+          title: Text('[DEBUG] Copy track UUID to clipboard'),
+          onTap: () {
+            Clipboard.setData(
+              ClipboardData(text: trackData.id!),
+            );
+            Navigator.of(context).pop();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Copied track UUID to clipboard'),
+                behavior: SnackBarBehavior.floating,
+              ),
+            );
+          },
+        )
+      ],
     );
   }
 }
