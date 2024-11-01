523688577f6b5b113da89b82339866bf18eb0bb7
sqz269 <yin2690855381@gmail.com>
2024-11-01 01:50:45 -0400

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tlmc_player_app/views/mobile/screens/album_screen_mobile.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tlmc_player_app/global_configuration.dart';
+import 'package:tlmc_player_app/models/oidc_configuration.dart';
+import 'package:tlmc_player_app/services/api/i_audio_service.dart';
+import 'package:tlmc_player_app/services/api/i_media_session_sevice.dart';
+import 'package:tlmc_player_app/services/api/i_playlist_service.dart';
+import 'package:tlmc_player_app/services/impl/api_client_provider.dart';
+import 'package:tlmc_player_app/services/impl/audio_services/just_audio_audio_service.dart';
+import 'package:tlmc_player_app/services/impl/audio_services/media_kit_audio_service.dart';
+import 'package:tlmc_player_app/services/impl/authentication_service.dart';
+import 'package:tlmc_player_app/services/impl/logging_service.dart';
+import 'package:tlmc_player_app/services/impl/media_session_services/cross_platform_media_session_service.dart';
+import 'package:tlmc_player_app/services/impl/media_session_services/windows_smtc_media_session_service.dart';
+import 'package:tlmc_player_app/services/impl/playlist_services/ondemand_playlist_service.dart';
+import 'package:tlmc_player_app/services/impl/queue_service.dart';
+import 'package:tlmc_player_app/services/impl/radio_service.dart';
+import 'package:tlmc_player_app/views/common/screen_size_dependent.dart';
+import 'package:tlmc_player_app/views/desktop/desktop_application.dart';
 
+import 'package:media_kit/media_kit.dart';
 final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
 final GlobalKey<NavigatorState> _sectionANavigatorKey =
@@ -15,7 +39,59 @@ final GlobalKey<NavigatorState> _sectionANavigatorKey =
 // i.e. navigation state is maintained separately for each item. This setup also
 // enables deep linking into nested pages.
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
+
+  // Logging First
+  Get.put<LoggingService>(LoggingService.createLogger());
+
+  var logger = Get.find<LoggingService>().getLogger("Initialization");
+
+  logger.i("Initializing application");
+
+  // Initialize audio service based on running platform
+  if (GetPlatform.isDesktop || GetPlatform.isWeb) {
+    logger.i("Desktop | Web platform detected. Using MediaKitAudioService.");
+    Get.put<IAudioService>(MediaKitAudioService());
+  } else {
+    logger.i("Mobile platform detected. Using JustAudioAudioService.");
+    Get.put<IAudioService>(JustAudioAudioService());
+  }
+
+  Get.put<QueueService>(QueueService());
+
+  // Initialize Media Session services based on running platform
+  if (GetPlatform.isWindows) {
+    logger.i("Windows platform detected. Using SMTC Media Session Service.");
+    Get.put<IMediaSessionService>(WindowsSmtcMediaSessionService());
+  } else {
+    logger.i(
+        "Non-Windows platform detected. Using Cross-platform Media Session service (Implementor audio_session package).");
+    Get.put<IMediaSessionService>(CrossPlatformMediaSessionService());
+    var audioHandler = await AudioService.init(
+      builder: () => Get.find<IMediaSessionService>() as BaseAudioHandler,
+      config: const AudioServiceConfig(
+        androidNotificationChannelId: 'com.sqz269.tlmc_player_app',
+        androidNotificationChannelName: 'Test',
+      ),
+    );
+  }
+
+  // Initialize routes
+  logger.i("Initializing routes");
+  // Initialize services
+  Get.put<OidcAuthenticationService>(OidcAuthenticationService(
+      oidcConfiguration: OidcConfiguration(
+    oidcRealmUrl: GlobalConfiguration.SSO_ENDPOINT,
+    clientId: GlobalConfiguration.SSO_CLIENT_ID,
+  )));
+
+  Get.put<ApiClientProvider>(ApiClientProvider());
+
+  Get.put<RadioService>(RadioService());
+  Get.put<IPlaylistService>(OndemandPlaylistService());
+
   runApp(NestedTabNavigationExampleApp());
 }
 
@@ -59,6 +135,13 @@ class NestedTabNavigationExampleApp extends StatelessWidget {
                     path: 'details',
                     builder: (BuildContext context, GoRouterState state) =>
                         const DetailsScreen(label: 'A'),
+                  ),
+                  GoRoute(
+                    path: 'album',
+                    builder: (context, state) {
+                      return AlbumScreenMobile(
+                          albumId: '2e0324ed-db9d-4266-8ca8-ae5d61e6d2c6');
+                    },
                   ),
                 ],
               ),
@@ -231,6 +314,13 @@ class RootScreen extends StatelessWidget {
                 GoRouter.of(context).go(detailsPath, extra: '$label-XYZ');
               },
               child: const Text('View details'),
+            ),
+            const Padding(padding: EdgeInsets.all(4)),
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).go('/a/album');
+              },
+              child: const Text('Album'),
             ),
             const Padding(padding: EdgeInsets.all(4)),
             if (secondDetailsPath != null)
