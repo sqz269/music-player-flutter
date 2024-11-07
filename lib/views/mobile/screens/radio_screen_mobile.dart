7770f48966bae05704079f7aeeac762ead7225ef
sqz269 <yin2690855381@gmail.com>
2024-11-07 00:22:35 -0500

    Automatically adjust viewport height based off keyboard and input state

--- lib/views/mobile/screens/radio_screen_mobile.dart
+++ lib/views/mobile/screens/radio_screen_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get/get.dart';
 import 'package:tlmc_player_app/services/impl/static_data_provider.dart';
 import 'package:tlmc_player_app/views/common/widget/date_input.dart';
@@ -9,72 +10,104 @@ class RadioScreenMobile extends StatefulWidget {
 
   DateTime? startRange;
   DateTime? endRange;
+  GlobalKey inputKey = GlobalKey();
 
   @override
   State<RadioScreenMobile> createState() => _RadioScreenMobileState();
 }
 
 class _RadioScreenMobileState extends State<RadioScreenMobile> {
+  var inputHeight = 0.0;
+  var keyboardHeight = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    // Subscribe to keyboard visibility changes
+    KeyboardVisibilityController().onChange.listen((bool visible) {
+      if (visible) {
+        setState(() {
+          keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+        });
+      } else {
+        setState(() {
+          keyboardHeight = 0;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final staticDataProvider = Get.find<StaticDataProvider>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Radio'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              DateInput(
-                controller: TextEditingController(),
-                label: 'Release Date (After)',
-                initialDate: widget.startRange,
-                firstDate: DateTime(1970, 1, 1),
-                lastDate: DateTime.now(),
-              ),
-              const SizedBox(height: 16),
-              DateInput(
-                controller: TextEditingController(),
-                label: 'Release Date (Before)',
-                initialDate: widget.endRange,
-                firstDate: DateTime(1970, 1, 1),
-                lastDate: DateTime.now(),
-              ),
-              const SizedBox(height: 16),
-              ChipsInput<String>(
-                decoration: InputDecoration(
-                  labelText: "Select Original Tracks",
+    return KeyboardVisibilityBuilder(
+      builder: (context, isKeyboardVisible) => Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          title: const Text('Radio'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: SingleChildScrollView(
+            // physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                DateInput(
+                  controller: TextEditingController(),
+                  label: 'Release Date (After)',
+                  initialDate: widget.startRange,
+                  firstDate: DateTime(1970, 1, 1),
+                  lastDate: DateTime.now(),
                 ),
-                initialValue: ["Test"],
-                chipBuilder: (context, state, string) {
-                  return InputChip(
-                    key: ObjectKey(string),
-                    label: Text(string),
-                    onDeleted: () => state.deleteChip(string),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  );
-                },
-                suggestionBuilder: (context, state, str) {
-                  return ListTile(
-                    key: ObjectKey(str),
-                    title: Text(str),
-                    onTap: () => state.selectSuggestion(str),
-                  );
-                },
-                findSuggestions: (String query) {
-                  return staticDataProvider.state?.originalTracks
-                          .map<String>((e) => e.title?.default_ ?? "")
-                          .toList() ??
-                      [];
-                },
-                onChanged: (data) {
-                  print(data);
-                },
-              ),
-            ],
+                const SizedBox(height: 16),
+                DateInput(
+                  controller: TextEditingController(),
+                  label: 'Release Date (Before)',
+                  initialDate: widget.endRange,
+                  firstDate: DateTime(1970, 1, 1),
+                  lastDate: DateTime.now(),
+                ),
+                const SizedBox(height: 16),
+                ChipsInput<String>(
+                  key: widget.inputKey,
+                  decoration: InputDecoration(
+                    labelText: "Select Original Tracks",
+                  ),
+                  initialValue: ["Test"],
+                  chipBuilder: (context, state, string) {
+                    return InputChip(
+                      key: ObjectKey(string),
+                      label: Text(string),
+                      onDeleted: () => state.deleteChip(string),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    );
+                  },
+                  suggestionBuilder: (context, state, str) {
+                    return ListTile(
+                      key: ObjectKey(str),
+                      title: Text(str),
+                      onTap: () => state.selectSuggestion(str),
+                    );
+                  },
+                  findSuggestions: (String query) {
+                    return staticDataProvider.state?.originalTracks
+                            .map<String>((e) => e.title?.default_ ?? "")
+                            .toList() ??
+                        [];
+                  },
+                  onChanged: (data) {
+                    inputHeight =
+                        widget.inputKey.currentContext?.size?.height ?? 0;
+                    print(inputHeight);
+                    print(keyboardHeight);
+                  },
+                ),
+                if (isKeyboardVisible)
+                  SizedBox(
+                    height: inputHeight + keyboardHeight,
+                  ),
+              ],
+            ),
           ),
         ),
       ),
